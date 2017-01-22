73094e591aaaba580d22ae56d46f9c953e282b1d
Massimo Hamilton <massimo.hamilton@outlook.com>
2017-01-22 07:44:50 +0000

    Updates to powershell profile

--- profile.ps1
+++ profile.ps1
@@ -17,35 +17,38 @@ function amend { git commit --amend }
 function gs { git status $args }
 function git_checkout { git checkout $args }
 Set-Alias gc git_checkout -Force -Option AllScope
-function gcf { git checkout q4-freeze }
+function gcm { git checkout master }
 function gr { git rebase $args }
+function gro { git rebase origin/master $args }
 function gru { git rebase upstream/master $args }
-function grw { git rebase upstream/q4-freeze $args }
 function gf { git fetch --all $args }
 function gb { git branch $args }
 function git_push { git push $args }
 Set-Alias gp git_push -Force -Option AllScope
-function dob {
+function rewrite {
+  git filter-branch --env-filter "export GIT_COMMITTER_NAME='Massimo Hamilton' && export GIT_COMMITTER_EMAIL=massimo.hamilton@outlook.com && export GIT_AUTHOR_NAME='Massimo Hamilton' && export GIT_AUTHOR_EMAIL=massimo.hamilton@outlook.com" -- --branches
+}
+function pr {
+  new-pull-request origin "/compare/master..."
+}
+function pru {
+  new-pull-request upstream "/compare/master...massimocode:"
+}
+function new-pull-request {
+  (git log HEAD...HEAD^)[4].Trim() | clip
+  $branchName = (git branch | Where-Object {$_.trim().indexOf("*") -eq 0}).Substring(2);
+  $prUrl = (git remote show $args[0])[1].Trim()
+  $prUrl = $prUrl.Substring(11, $prUrl.Length - 15) + $args[1] + $branchName;
+  Start-Process microsoft-edge:$($prUrl);
+}
+function deleteoldbranches
+{
   git remote prune origin
   git branch --merged |
     ForEach-Object { $_.Trim() } |
     Where-Object {$_ -NotMatch "^\*"} |
     Where-Object {-not ( $_ -Like "*master" )} |
     ForEach-Object { git branch -d $_ }
-}
-function rewrite {
-  git filter-branch --env-filter "export GIT_COMMITTER_NAME='Massimo Hamilton' && export GIT_COMMITTER_EMAIL=massimo.hamilton@outlook.com && export GIT_AUTHOR_NAME='Massimo Hamilton' && export GIT_AUTHOR_EMAIL=massimo.hamilton@outlook.com" -- --branches
-}
-function prg {
-  new-pull-request "/compare/master...massimocode:"
-}
-function new-pull-request {
-  (git log HEAD...HEAD^)[4].Trim() | clip
-  $branchName = (git branch | Where-Object {$_.trim().indexOf("*") -eq 0}).Substring(2);
-  $prUrl = (git remote show upstream)[1].Trim()
-  $prUrl = $prUrl.Substring(11, $prUrl.Length - 15) + $args[0] + $branchName;
-  echo $prUrl
-  Start-Process microsoft-edge:$($prUrl);
 }
 
 # Navigation
@@ -57,7 +60,7 @@ function ep { code C:\projects\settings-and-config\profile.ps1 }
 function epl { code $env:userprofile\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1 }
 function code { code-insiders $args }
 function gulp { node node_modules/gulp/bin/gulp $args }
-function typings { node node_modules/typings/dist/bin $args }
+function tslint { node node_modules/tslint/bin/tslint $args }
 function tsc { node node_modules/typescript/bin/tsc $args }
 function msbuild { & "C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe" $args }
 function open {
@@ -77,3 +80,8 @@ function p {
   ping www.google.co.uk -t
 }
 function hosts { code c:\windows\system32\drivers\etc\hosts }
+function renameall {
+  $ext1 = $args[0];
+  $ext2 = $args[1];
+  Get-ChildItem ('*.' + $ext1) | Rename-Item -newname {  $_.name  -replace ("." + $ext1), ("." + $ext2)  }
+}
