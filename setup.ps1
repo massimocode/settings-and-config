abd9bfdba9ccc93f2575e711812603ac943fa8fc
Massimo Hamilton <massimo.hamilton@outlook.com>
2018-01-15 11:05:16 +0000

    Create powershell directory

--- setup.ps1
+++ setup.ps1
@@ -1,3 +1,4 @@
+mkdir -Force "$env:userprofile\Documents\WindowsPowerShell"
 Write-Output ". C:\projects\settings-and-config\profile.ps1" >> "$env:userprofile\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1"
 npm config set package-lock false
 git config --global include.path "C:\projects\settings-and-config\.gitconfig"
